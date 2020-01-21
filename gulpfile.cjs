28196166228b13bf8746eeeff7be33784fe74b0a
Cédric Belin <cedric@belin.io>
2020-01-21 20:13:19 +0100

    Added a build system based on Gulp

---
+++ gulpfile.cjs
@@ -0,0 +1,69 @@
+const {spawn} = require('child_process');
+const del = require('del');
+const {promises} = require('fs');
+const {series, task, watch} = require('gulp');
+const {delimiter, normalize, resolve} = require('path');
+
+// Initialize the build system.
+const _path = 'PATH' in process.env ? process.env.PATH : '';
+const _vendor = resolve('node_modules/.bin');
+if (!_path.includes(_vendor)) process.env.PATH = `${_vendor}${delimiter}${_path}`;
+
+/** Builds the project. */
+task('build', () => _exec('tsc', ['--project', 'src/tsconfig.json']));
+
+/** Deletes all generated files and reset any saved state. */
+task('clean', () => del(['build', 'doc/api', 'lib', 'var/**/*', 'web']));
+
+/** Uploads the results of the code coverage. */
+task('coverage', () => _exec('coveralls', ['var/lcov.info']));
+
+/** Builds the documentation. */
+task('doc', async () => {
+  for (const path of ['CHANGELOG.md', 'LICENSE.md']) await promises.copyFile(path, `doc/about/${path.toLowerCase()}`);
+  await _exec('typedoc', ['--gaID', process.env.GOOGLE_ANALYTICS_ID, '--options', 'etc/typedoc.json']);
+  await _exec('mkdocs', ['build', '--config-file=doc/mkdocs.yaml']);
+  return del(['doc/about/changelog.md', 'doc/about/license.md', 'web/mkdocs.yaml']);
+});
+
+/** Fixes the coding standards issues. */
+task('fix', () => _exec('eslint', ['--config=etc/eslint.yaml', '--fix', 'src/**/*.ts']));
+
+/** Performs the static analysis of source code. */
+task('lint', () => _exec('eslint', ['--config=etc/eslint.yaml', 'src/**/*.ts']));
+
+/** Runs the test suites. */
+const mocha = ['node_modules/.bin/mocha', '--recursive'];
+task('test:run', () => _exec('c8', ['--all', '--include=lib/**/*.js', '--report-dir=var', '--reporter=lcovonly', ...mocha]));
+task('test', series('build', 'test:run'));
+
+/** Upgrades the project to the latest revision. */
+task('upgrade', async () => {
+  await _exec('git', ['reset', '--hard']);
+  await _exec('git', ['fetch', '--all', '--prune']);
+  await _exec('git', ['pull', '--rebase']);
+  await _exec('npm', ['install', '--ignore-scripts', '--production=false']);
+  return _exec('npm', ['update', '--dev']);
+});
+
+/** Watches for file changes. */
+task('watch', () => {
+  watch('src/**/*.ts', {ignoreInitial: false}, task('build'));
+  watch('test/**/*.js', task('test'));
+});
+
+/** Runs the default tasks. */
+task('default', task('build'));
+
+/**
+ * Spawns a new process using the specified command.
+ * @param {string} command The command to run.
+ * @param {string[]} args The command arguments.
+ * @param {object} options The settings to customize how the process is spawned.
+ * @return {Promise<void>} Completes when the command is finally terminated.
+ */
+function _exec(command, args = [], options = {}) {
+  return new Promise((fulfill, reject) => spawn(normalize(command), args, {shell: true, stdio: 'inherit', ...options})
+    .on('close', code => code ? reject(new Error(`${command}: ${code}`)) : fulfill())
+  );
+}
