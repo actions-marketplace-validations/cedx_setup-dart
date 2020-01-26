d59787047ad18e1269d63fb323f13ff7721567de
Cédric Belin <cedric@belin.io>
2020-01-26 20:35:31 +0100

    Added ARM architecture

--- src/dart_sdk.ts
+++ src/dart_sdk.ts
@@ -6,10 +6,16 @@ import {format} from 'util';
 /** Defines the architecture of the Dart SDK. */
 export enum Architecture {
 
-  /** Specifies a 32-bit Dart SDK. */
+  /** Specifies a 32-bit ARM architecture. */
+  arm = 'arm',
+
+  /** Specifies a 64-bit ARM architecture. */
+  arm64 = 'arm64',
+
+  /** Specifies a 32-bit AMD/Intel architecture. */
   ia32 = 'ia32',
 
-  /** Specifies a 64-bit Dart SDK. */
+  /** Specifies a 64-bit AMD/Intel architecture. */
   x64 = 'x64'
 }
 
