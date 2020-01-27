0ea3d1194dc7931222d4188a80634877943447a5
Cédric Belin <cedric@belin.io>
2020-01-27 18:00:21 +0100

    Code optimization

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,8 @@ import {Architecture, DartSdk, ReleaseChannel} from './dart_sdk';
  * @return Completes when the Dart SDK is installed.
  */
 export async function main(): Promise<void> {
-  const architecture = getInput('architecture') || arch();
-  const releaseChannel = getInput('release-channel') || ReleaseChannel.stable;
+  const architecture = (getInput('architecture') || arch()) as Architecture;
+  const releaseChannel = (getInput('release-channel') || ReleaseChannel.stable) as ReleaseChannel;
   const version = getInput('version') || 'latest';
-
-  return new DartSdk({
-    architecture: getInput('architecture') as Architecture,
-    releaseChannel: getInput('release-channel') as ReleaseChannel,
-    version: getInput('version')
-  }).install();
+  return new DartSdk({architecture, releaseChannel, version}).install();
 }
