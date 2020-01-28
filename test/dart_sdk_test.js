3b18f37093b52666cd571998f45b826c6bb2f3b3
Cédric Belin <cedric@belin.io>
2020-01-28 09:55:34 +0100

    Fixed the tests

--- test/dart_sdk_test.js
+++ test/dart_sdk_test.js
@@ -13,16 +13,16 @@ describe('DartSdk', function() {
       const dartSdk = new DartSdk;
       const urlPattern = `https://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-%s-x64-release.zip`;
       if (process.platform == 'darwin') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, 'macos'));
-      else if (process.platform == 'linux') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, 'linux'));
       else if (process.platform == 'win32') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, 'windows'));
+      else expect(dartSdk.releaseUrl).to.equal(format(urlPattern, 'linux'));
     });
 
     it('should point to a valid Dart SDK release', () => {
       const dartSdk = new DartSdk({architecture: Architecture.ia32, releaseChannel: ReleaseChannel.dev, version: '12.34.56-dev.7.8'});
       const urlPattern = `https://storage.googleapis.com/dart-archive/channels/dev/release/12.34.56-dev.7.8/sdk/dartsdk-%s-ia32-release.zip`;
       if (process.platform == 'darwin') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, 'macos'));
-      else if (process.platform == 'linux') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, 'linux'));
       else if (process.platform == 'win32') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, 'windows'));
+      else expect(dartSdk.releaseUrl).to.equal(format(urlPattern, 'linux'));
     });
   });
 
@@ -37,8 +37,9 @@ describe('DartSdk', function() {
 
   describe('#install()', () => {
     it('should add the Dart SDK binaries to the PATH environment variable', async () => {
-      await new DartSdk().install();
-      expect(process.env.PATH).to.contain(normalize('/dart-sdk/bin'));
+      const dartSdk = new DartSdk;
+      await dartSdk.install();
+      expect(process.env.PATH).to.contain(normalize(`/dart-sdk/${dartSdk.version}/${dartSdk.architecture}/bin`));
     });
   });
 });
