18024bc9089811a61b97197011a5a007e4b33370
Cédric Belin <cedric@belin.io>
2020-03-24 15:38:40 +0100

    Replaced `chai` by Node `assert` for testing

--- test/dart_sdk_test.js
+++ test/dart_sdk_test.js
@@ -1,4 +1,4 @@
-const {expect} = require('chai');
+const {strict: assert} = require('assert');
 const {existsSync, promises} = require('fs');
 const {normalize, join} = require('path');
 const {format} = require('util');
@@ -17,17 +17,17 @@ describe('DartSdk', function() {
     it('should point, by default, to the latest stable release', () => {
       const dartSdk = new DartSdk;
       const urlPattern = `https://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-%s-x64-release.zip`;
-      if (process.platform == 'darwin') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, Platform.macos));
-      else if (process.platform == 'win32') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, Platform.windows));
-      else expect(dartSdk.releaseUrl).to.equal(format(urlPattern, Platform.linux));
+      if (process.platform == 'darwin') assert.equal(dartSdk.releaseUrl, format(urlPattern, Platform.macos));
+      else if (process.platform == 'win32') assert.equal(dartSdk.releaseUrl, format(urlPattern, Platform.windows));
+      else assert.equal(dartSdk.releaseUrl, format(urlPattern, Platform.linux));
     });
 
     it('should point to a valid Dart SDK release', () => {
       const dartSdk = new DartSdk({architecture: Architecture.ia32, releaseChannel: ReleaseChannel.dev, version: '12.34.56-dev.7.8'});
       const urlPattern = `https://storage.googleapis.com/dart-archive/channels/dev/release/12.34.56-dev.7.8/sdk/dartsdk-%s-ia32-release.zip`;
-      if (process.platform == 'darwin') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, Platform.macos));
-      else if (process.platform == 'win32') expect(dartSdk.releaseUrl).to.equal(format(urlPattern, Platform.windows));
-      else expect(dartSdk.releaseUrl).to.equal(format(urlPattern, Platform.linux));
+      if (process.platform == 'darwin') assert.equal(dartSdk.releaseUrl, format(urlPattern, Platform.macos));
+      else if (process.platform == 'win32') assert.equal(dartSdk.releaseUrl, format(urlPattern, Platform.windows));
+      else assert.equal(dartSdk.releaseUrl, format(urlPattern, Platform.linux));
     });
   });
 
@@ -35,8 +35,8 @@ describe('DartSdk', function() {
     it('should properly download and extract the Dart SDK', async () => {
       const executable = process.platform == 'win32' ? 'dart.exe' : 'dart';
       const sdkDir = await new DartSdk({releaseChannel: ReleaseChannel.stable, version: '2.7.0'}).download();
-      expect(existsSync(join(sdkDir, `bin/${executable}`))).to.be.true;
-      expect((await promises.readFile(join(sdkDir, 'version'), 'utf8')).trim()).to.equal('2.7.0');
+      assert(existsSync(join(sdkDir, `bin/${executable}`)));
+      assert.equal((await promises.readFile(join(sdkDir, 'version'), 'utf8')).trim(), '2.7.0');
     });
   });
 
@@ -44,7 +44,7 @@ describe('DartSdk', function() {
     it('should add the Dart SDK binaries to the PATH environment variable', async () => {
       const dartSdk = new DartSdk;
       await dartSdk.install();
-      expect(process.env.PATH).to.contain(normalize(`/dart-sdk/${dartSdk.version}/${dartSdk.architecture}/bin`));
+      assert(process.env.PATH.includes(normalize(`/dart-sdk/${dartSdk.version}/${dartSdk.architecture}/bin`)));
     });
   });
 });
