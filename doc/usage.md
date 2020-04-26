b36ba4cafa56266cbcf2cdb4e9156588f429795c
Cédric Belin <cedric@belin.io>
2020-04-26 15:20:11 +0200

    Updated the documentation

--- doc/usage.md
+++ doc/usage.md
@@ -45,7 +45,7 @@ steps:
 ```
 
 !!! tip
-    A sample workflow can be found in the [`build.yaml`](https://github.com/cedx/setup-dart/blob/master/example/build.yaml) file.
+    A sample workflow can be found in the [`build.yaml`](https://git.belin.io/cedx/setup-dart/src/branch/master/example/build.yaml) file.
 
 ### Matrix
 Setup multiple versions of the Dart SDK on multiple operating systems:
--- gulpfile.cjs
+++ gulpfile.cjs
@@ -1,12 +1,12 @@
 const {spawn} = require('child_process');
 const del = require('del');
-const {promises, readFileSync} = require('fs');
+const {readFileSync} = require('fs');
 const {dest, series, src, task, watch} = require('gulp');
 const replace = require('gulp-replace');
 const {delimiter, normalize, resolve} = require('path');
 
 // Initialize the build system.
-const _path = 'PATH' in process.env ? process.env.PATH : '';
+const _path = process.env.PATH ?? '';
 const _vendor = resolve('node_modules/.bin');
 if (!_path.includes(_vendor)) process.env.PATH = `${_vendor}${delimiter}${_path}`;
 
@@ -23,10 +23,8 @@ task('coverage', () => _exec('coveralls', ['var/lcov.info']));
 
 /** Builds the documentation. */
 task('doc', async () => {
-  for (const path of ['CHANGELOG.md', 'LICENSE.md']) await promises.copyFile(path, `doc/about/${path.toLowerCase()}`);
-  await _exec('typedoc', ['--gaID', process.env.GOOGLE_ANALYTICS_ID, '--options', 'etc/typedoc.json']);
-  await _exec('mkdocs', ['build', '--config-file=doc/mkdocs.yaml']);
-  return del(['doc/about/changelog.md', 'doc/about/license.md', 'www/mkdocs.yaml']);
+  await _exec('typedoc', ['--options', 'etc/typedoc.json']);
+  return _exec('mkdocs', ['build', '--config-file=etc/mkdocs.yaml']);
 });
 
 /** Fixes the coding standards issues. */
