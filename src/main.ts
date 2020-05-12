cceb57fb3a24b44e532e42684f29f2236fbe9c3a
Cédric Belin <cedric@belin.io>
2020-05-13 00:41:38 +0200

    Fixed the warnings of the static analyzer

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,12 @@ import {Architecture, DartSdk, ReleaseChannel} from './dart_sdk.js';
  * @return Completes when the Dart SDK is installed.
  */
 export async function main(): Promise<void> {
+  /* eslint-disable @typescript-eslint/no-unnecessary-condition */
   const architecture = (getInput('architecture') || arch()) as Architecture;
-  const releaseChannel = (getInput('release-channel') || ReleaseChannel.stable) as ReleaseChannel;
+  const releaseChannel = (getInput('release-channel') || 'stable') as ReleaseChannel;
   const version = getInput('version') || 'latest';
+  /* eslint-enable @typescript-eslint/no-unnecessary-condition */
+
   return new DartSdk({architecture, releaseChannel, version}).install();
 }
 
