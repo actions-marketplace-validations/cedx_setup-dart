f56c4e206943f0c734c02b6cc6c637feba057aec
Cédric Belin <cedric@belin.io>
2020-05-19 22:03:53 +0200

    Updated the build system

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -5,5 +5,5 @@ Set-Location (Split-Path $PSScriptRoot)
 tool/clean.ps1
 tool/version.ps1
 haxe --no-traces build.hxml
-node_modules/.bin/ncc build lib/setup_dart.js --minify --out=var
+node_modules/.bin/ncc build bin/setup_dart.js --minify --out=var
 @("#!/usr/bin/env node") + (Get-Content var/index.js) | Out-File bin/setup_dart.js
