2da07298bee895b14de9d7e89bd29e76949d491b
Cédric Belin <cedric@belin.io>
2020-05-21 00:08:22 +0200

    Code formatting

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -5,5 +5,6 @@ Set-Location (Split-Path $PSScriptRoot)
 tool/clean.ps1
 tool/version.ps1
 haxe --no-traces build.hxml
+
 node_modules/.bin/ncc build bin/setup_dart.js --minify --out=var
 @("#!/usr/bin/env node") + (Get-Content var/index.js) | Out-File bin/setup_dart.js
