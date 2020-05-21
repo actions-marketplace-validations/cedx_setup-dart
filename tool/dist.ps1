a261ee40f1938116bc8c9d98816895b56abd1f01
Cédric Belin <cedric@belin.io>
2020-05-22 01:23:39 +0200

    Updated the "dist" task

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -8,3 +8,4 @@ haxe --no-traces build.hxml
 
 node_modules/.bin/ncc build bin/setup_dart.js --minify --out=var
 @("#!/usr/bin/env node") + (Get-Content var/index.js) | Out-File bin/setup_dart.js
+if (-not $IsWindows) { chmod +x bin/setup_dart.js }
