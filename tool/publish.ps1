368e80c77c9706defff91dc1a493751f9f9249a3
Cédric Belin <cedric@belin.io>
2020-05-22 00:46:23 +0200

    Added a "publish" task

---
+++ tool/publish.ps1
@@ -0,0 +1,7 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+Set-Location (Split-Path $PSScriptRoot)
+
+$version = (Get-Content haxelib.json | ConvertFrom-Json).version
+git tag "v$version"
+git push origin "v$version"
