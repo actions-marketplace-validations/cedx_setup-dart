e360037ab677edc987893fc823998ce3b52336eb
Cédric Belin <cedric@belin.io>
2020-06-01 01:26:28 +0200

    Fixed the collection of code coverage

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -1,21 +1,28 @@
 import mcover.coverage.MCoverage;
 import mcover.coverage.client.LcovPrintClient;
 import setup_dart.*;
-import utest.UTest;
+import utest.Runner;
+import utest.ui.Report;
 
-/** Runs the test suites. **/
+/** Runs the test suite. **/
 class TestAll {
+
+	/** The test cases. **/
+	static final tests = [
+		new DartSdkTest()
+	];
 
 	/** Application entry point. **/
 	static function main(): Void {
-		final tests = [
-			new DartSdkTest()
-		];
-
-		UTest.run(tests, () -> {
+		final runner = new Runner();
+		runner.onComplete.add(_ -> {
 			final logger = MCoverage.getLogger();
 			logger.addClient(new LcovPrintClient("setup_dart", "var/lcov.info"));
 			logger.report();
 		});
+
+    Report.create(runner);
+		for (test in tests) runner.addCase(test);
+    runner.run();
 	}
 }
