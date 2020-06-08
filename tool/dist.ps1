cd6c0bbf9d0b99367af654b8deee1b5389b16fdb
Cédric Belin <cedric@belin.io>
2020-06-08 20:40:55 +0200

    Updated the build system

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -6,6 +6,6 @@ tool/clean.ps1
 tool/version.ps1
 haxe build.hxml
 
-node_modules/.bin/ncc build bin/setup_dart.js --minify --out=var
+npm run dist
 @("#!/usr/bin/env node") + (Get-Content var/index.js) | Out-File bin/setup_dart.js
 if (-not $IsWindows) { chmod +x bin/setup_dart.js }
