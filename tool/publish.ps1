24d0f21b8d234be80b91324426f19e121e5dc403
Cédric Belin <cedric@belin.io>
2020-06-12 10:19:49 +0200

    Updated the build system

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -2,6 +2,8 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
+tool/dist.ps1
+
 $version = (Get-Content haxelib.json | ConvertFrom-Json).version
 git tag "v$version"
 git push origin "v$version"
