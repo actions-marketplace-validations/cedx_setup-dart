80cab950fce981e84f1c472b34b9ccd6bad7ae82
Cédric Belin <cedric@belin.io>
2020-12-03 09:58:07 +0100

    Updated the build system

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -6,15 +6,13 @@ $version = (Get-Content haxelib.json | ConvertFrom-Json).version
 haxe --define doc-gen --xml var/api.xml build.hxml
 haxelib run dox `
 	--define description "Set up your GitHub Actions workflow with a specific version of the Dart SDK." `
-	--define logo "https://api.belin.io/setup-dart/favicon.ico" `
 	--define source-path "https://git.belin.io/cedx/setup-dart/src/branch/main/src" `
-	--define themeColor 0xffec3d `
+	--define themeColor 0x0175c2 `
 	--define version $version `
 	--define website "https://belin.io" `
 	--input-path var `
-	--output-path doc/api `
+	--output-path docs/api `
 	--title "Setup Dart Action" `
 	--toplevel-package setup_dart
 
-Copy-Item doc/img/favicon.ico doc/api
-mkdocs build --config-file=etc/mkdocs.yaml
+Copy-Item docs/favicon.ico docs/api
