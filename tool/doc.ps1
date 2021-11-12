8f5b43079f4ebdd9272430f510791681a9428d5c
Cédric Belin <cedric@belin.io>
2021-11-12 21:01:45 +0100

    Updated the API reference

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -2,19 +2,19 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-if (Test-Path docs/api) { Remove-Item docs/api -Force -Recurse }
+if (Test-Path docs) { Remove-Item docs -Force -Recurse }
 
 $version = (Get-Content haxelib.json | ConvertFrom-Json).version
 haxe --define doc-gen --xml var/api.xml build.hxml
 lix run dox `
 	--define description "Set up your GitHub Actions workflow with a specific version of the Dart SDK." `
-	--define source-path "https://github.com/cedx/setup-dart/blob/main/src" `
+	--define source-path "https://bitbucket.org/cedx/setup-dart/src/main/src" `
 	--define themeColor 0x0175c2 `
 	--define version $version `
-	--define website "https://cedx.github.io/setup-dart" `
+	--define website "https://bitbucket.org/cedx/setup-dart" `
 	--input-path var `
-	--output-path docs/api `
+	--output-path docs `
 	--title "Setup Dart Action" `
 	--toplevel-package setup_dart
 
-Copy-Item docs/favicon.ico docs/api
+Copy-Item www/favicon.ico docs
