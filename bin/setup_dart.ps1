2c1f0e8e9d093e5ada7ba11692b56fb2693dce99
Cédric Belin <cedric@belin.io>
2022-04-07 23:17:17 +0200

    Replaced the build system based on PowerShell by Lix scripts

--- bin/setup_dart.ps1
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-node "$PSScriptRoot/setup_dart.js" @args
--- tool/publish.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-Set-Location (Split-Path $PSScriptRoot)
-
-tool/dist.ps1
-
-$version = (Get-Content haxelib.json | ConvertFrom-Json).version
-git tag "v$version"
-git push origin "v$version"
